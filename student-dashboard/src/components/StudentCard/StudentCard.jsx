4a7c156676dda0663ccd2a00e43ee0c4c8103e79
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-10 15:42:00 +0200

    Add switch statement to add emojis to the status of each student conditionally.

--- student-dashboard/src/components/StudentCard/StudentCard.jsx
+++ student-dashboard/src/components/StudentCard/StudentCard.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './StudentCard.css'
 function StudentCard({name, grade, status}) {
+    switch (status) {
+        case 'active':
+            status = status + "🟢";
+            break;
+        
+        case 'graduated':
+            status = status + "🎓";
+            break;
+
+        case 'suspended':
+            status = status + "❌";
+            break;
+    
+        default:
+            break;
+    }
   return (
     <div className='student-card'>
         <div>{name}</div>
         <div>{grade}</div>
         <div>{status}</div>
+        
+return <li className="item">{name}</li>;
     </div>
   )
 }
