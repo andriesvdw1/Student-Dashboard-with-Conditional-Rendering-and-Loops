7dd22447f7d2829178d12c7cf5cebf1af6c4493a
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-10 16:01:33 +0200

    Add a condition where student grade values will display in green when larger than or equal to 75, and orange otherwise.

--- student-dashboard/src/components/StudentCard/StudentCard.jsx
+++ student-dashboard/src/components/StudentCard/StudentCard.jsx
@@ -20,7 +20,9 @@ function StudentCard({name, grade, status}) {
   return (
     <div className='student-card'>
         <div>{name}</div>
-        <div>{grade}</div>
+        
+        <div style={{ color: grade >= 75 ? 'green' : 'orange' }}>{grade}</div>
+        
         <div>{status}</div>
      </div>
   )
